c186f481273075e835e5844dd6404fc6c18dd4c0
Orinda Harrison <ajuongharry35@gmail.com>
2020-08-09 02:13:03 +0300

    Implement ArticleRepository

--- app/src/main/java/com/xently/news/data/repository/ArticlesRepository.kt
+++ app/src/main/java/com/xently/news/data/repository/ArticlesRepository.kt
@@ -1,43 +1,55 @@
 package com.xently.news.data.repository
 
 import com.xently.common.data.Source
-import com.xently.common.data.TaskResult
+import com.xently.common.data.Source.LOCAL
+import com.xently.common.data.Source.REMOTE
+import com.xently.common.data.data
+import com.xently.common.data.listData
 import com.xently.news.data.model.Article
 import com.xently.news.data.source.IArticleDataSource
 import com.xently.news.di.qualifiers.LocalArticlesDataSource
 import com.xently.news.di.qualifiers.RemoteArticlesDataSource
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class ArticlesRepository @Inject constructor(
     @LocalArticlesDataSource
-    local: IArticleDataSource,
+    private val local: IArticleDataSource,
     @RemoteArticlesDataSource
-    remote: IArticleDataSource,
-    ioDispatcher: CoroutineDispatcher = Dispatchers.IO
+    private val remote: IArticleDataSource,
+    private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO
 ) : IArticlesRepository {
-    override suspend fun saveArticles(vararg articles: Article): TaskResult<List<Article>> {
-        TODO("Not yet implemented")
+    override suspend fun saveArticles(vararg articles: Article) = withContext(ioDispatcher) {
+        remote.saveArticles(*articles).run {
+            local.saveArticles(*listData.toTypedArray())
+        }
     }
 
-    override suspend fun getArticles(searchQuery: String?): TaskResult<List<Article>> {
-        TODO("Not yet implemented")
+    override suspend fun getArticles(searchQuery: String?) = withContext(ioDispatcher) {
+        remote.getArticles(searchQuery).run {
+            local.saveArticles(*listData.toTypedArray())
+        }
     }
 
-    override suspend fun getArticle(id: Long): TaskResult<Article> {
-        TODO("Not yet implemented")
+    override suspend fun getArticle(id: Long) = withContext(ioDispatcher) {
+        remote.getArticle(id).run {
+            data?.also { article ->
+                local.saveArticles(article)
+            }
+            this
+        }
     }
 
-    override suspend fun getObservableArticles(
-        searchQuery: String?,
-        source: Source
-    ): Flow<List<Article>> {
-        TODO("Not yet implemented")
-    }
+    override suspend fun getObservableArticles(searchQuery: String?, source: Source) =
+        when (source) {
+            REMOTE -> remote.getObservableArticles(searchQuery, source)
+            LOCAL -> local.getObservableArticles(searchQuery, source)
+        }
 
-    override suspend fun getObservableArticle(id: Long, source: Source): Flow<Article> {
-        TODO("Not yet implemented")
+    override suspend fun getObservableArticle(id: Long, source: Source) = when (source) {
+        REMOTE -> remote.getObservableArticle(id, source)
+        LOCAL -> local.getObservableArticle(id, source)
     }
 }
